82df9c757e5d4f2e367421cf60298d79ecbc254d
ajayraj004 <ajayrajospf2@gmail.com>
2024-08-30 12:05:47 +0530

    update app.py

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
 from flask import Flask,  render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-    # return 'Hello, World!'
+@app.route('/index2.html')
+def home():
+    return render_template('index2.html')
 
-@app.route('/products')
-def products():
-    return 'this is product page'
+
+@app.route('/aboutus.html')
+def about():
+    return render_template('aboutus.html')
 
 
 if __name__ == "__main__":
